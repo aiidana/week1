0c10f0e0713ce6da6274989151a70ad090fffade
aidana <aidana05.ak@gmail.com>
2023-01-19 23:16:36 +0600

    first commit

---
+++ p_booleans.py
@@ -0,0 +1,28 @@
+#booleans: true or false
+
+print(10 > 9)#true
+print(10 == 9)#false
+print(10 < 9)#false
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+def myFunction() :
+  return True
+print(myFunction())
+
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")#yes
+
