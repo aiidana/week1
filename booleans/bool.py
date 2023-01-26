5fa03e5d397ecc3d76410983bf754c341ef72424
Aidana Koshkinbayeva <118048458+aiidana@users.noreply.github.com>
2023-01-26 14:56:15 +0600

    Add files via upload

---
+++ booleans/bool.py
@@ -0,0 +1,29 @@
+#true or false
+
+
+
+print(10 > 9)#true
+print(10 == 9)#false
+print(10 < 9)#false
+
+c = 230
+d = 34
+if d > c:
+  print("d is greater than c")
+else:
+  print("d is not greater than c")
+
+
+def Function() :
+  return True
+print(Function())
+
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")#yes
