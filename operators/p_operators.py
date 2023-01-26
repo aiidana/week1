3736617025ce82de60b203aef1ae3da44cf2ebc8
Aidana Koshkinbayeva <118048458+aiidana@users.noreply.github.com>
2023-01-26 15:43:46 +0600

    Add files via upload

---
+++ operators/p_operators.py
@@ -0,0 +1,50 @@
+#operators
+
+#addition
+x=5
+x=x+2 #same as x+=2
+print(x)
+
+
+#substraction
+x=3
+x=x-3 #same as x-=3
+
+#multiplication
+x=2
+x=x*4 #same as x*=4
+
+#division
+x=8
+x=x/3 #same as x/=3 (output = 2.666666)
+#division without remainder
+x=x//3 #same as x//=3 (output =2)
+
+
+#moduls
+x=10
+x=x%3 #same as x%=3 (output = 1)
+
+#Exponentiation
+x=3
+x=x**3 #or x**=3 (output=27)
+
+
+#Python Logical Operators
+#binary and, or, xor, not (&,|,^,~)
+x=x&3
+x=x^3
+x=x|3
+
+"""
+==	Equal	                  x == y	
+!=	Not equal	              x != y	
+>	Greater than	          x > y	
+<	Less than	              x < y	
+>=	Greater than or equal to  x >= y	
+<=	Less than or equal to	  x <= y	
+
+"""
+
+
+
